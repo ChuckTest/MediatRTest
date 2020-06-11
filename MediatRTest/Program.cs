624739aacf3ccc10279278b6e0224184d8f6aa60
Chuck LU <chuck.lu@edenred.com>
2020-06-11 13:44:43 +0800

    add setup code along with autofac https://github.com/jbogard/MediatR/wiki

---
+++ MediatRTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using MediatR;
+using System;
+using System.Reflection;
+
+namespace MediatRTest
+{
+    class MyType
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            // Uncomment to enable polymorphic dispatching of requests, but note that
+            // this will conflict with generic pipeline behaviors
+            // builder.RegisterSource(new ContravariantRegistrationSource());
+
+            // Mediator itself
+            builder
+                .RegisterType<Mediator>()
+                .As<IMediator>()
+                .InstancePerLifetimeScope();
+
+            // request & notification handlers
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            // finally register our custom code (individually, or via assembly scanning)
+            // - requests & handlers as transient, i.e. InstancePerDependency()
+            // - pre/post-processors as scoped/per-request, i.e. InstancePerLifetimeScope()
+            // - behaviors as transient, i.e. InstancePerDependency()
+            builder.RegisterAssemblyTypes(typeof(MyType).GetTypeInfo().Assembly).AsImplementedInterfaces(); // via assembly scan
+            //builder.RegisterType<MyHandler>().AsImplementedInterfaces().InstancePerDependency();          // or individually
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
